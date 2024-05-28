d4d112207978576629216c948a6197779e786773
Matthew Markfort <matthew.markfort@gmail.com>
2024-05-28 13:48:51 -0500

    Stub wish.c

--- wish.c
+++ wish.c
@@ -1,1 +1,17 @@
- 
+/*
+ * author    Matthew Markfort <matthew.markfort@my.metrostate.edu>
+ * class     ICS-462 Operating Systems (Summer 2024), Metropolitan State University
+ * date      28 May 2024
+ * due       22 Jul 2024
+ *
+ * "wisconsin shell" (wish) is a rudimentary Linux shell for learning purposes
+ *
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main(int argc, char *argv[]){
+    return EXIT_SUCCESS;
+}
