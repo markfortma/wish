c8aba1b74d2724f08d7541a9980611a9dcb758b1
Matthew Markfort <matthew.markfort@gmail.com>
2024-05-31 15:53:38 -0500

    Add item to .gitignore
    Compiles ok

--- wish.c
+++ wish.c
@@ -18,35 +18,45 @@
 #include<stdlib.h>
 #include<unistd.h>
 
+#define MAX_PATH_SIZE 16
 static
-char *PATH[] = {
+char *path[MAX_PATH_SIZE] = {
   "/bin"
 };
 
-void wish_execute(FILE *script){
-  char *line;
-  size_t linelen;
-  ssize_t nbytes;
-  while(!feof(script)){
-    nbytes = getline(&line, &linelen, script);
-    if(line[0] == '#'){
-      /* skip lines that start with # */
-      continue;
-    }
+static
+int path_count = 1;
+
+void lsh_execute(FILE *in){
+  char *line = NULL;
+  size_t linelen = 0;
+  ssize_t nbytes = 0;
+  while(!feof(in)){
+    if(in == stdin)
+      printf("wish> ");
+    nbytes = getline(&line, &linelen, in);
+
+    // exit while if no bytes read
+    if(0 == nbytes) break;
+    // skip on comment lines
+    if('#' == line[0]) continue;
+
     
-  } 
+  }
+  /* release the line buffer */
+  free(line);
 }
 
 int main(int argc, char *argv[]){
   if(2 == argc){
-      FILE *in = fopen(argv[1], "r");
-      wish_execute(in);
-      fclose(in);
+    FILE *in = fopen(argv[1], "r");
+    lsh_execute(in);
+    fclose(in);
   } else if(1 == argc){
-    wish_execute(stdin);
+    lsh_execute(stdin);
+  } else {
+    fprintf(stderr, "Usage: %s [script.sh]\n");
+    return EXIT_FAILURE;
   }
-  else{
-      fprintf(stderr, "Usage: %s script.sh\n", argv[0]);
-  }
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
